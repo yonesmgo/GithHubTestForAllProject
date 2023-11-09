e843c5fe559e64754b3e9a5656876e7fbdcf9b96
masoodtav <masoodtav@gmail.com>
2023-11-09 14:01:14 +0330

    masood

--- GithHubTestForAllProject/Controllers/HomeController.cs
+++ GithHubTestForAllProject/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace GithHubTestForAllProject.Controllers
             return View();
         }
 
+        public IActionResult Privacy1()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
